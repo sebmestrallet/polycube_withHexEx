4678f80c852b30898fe7ff106a02b40b02676526
Sébastien Mestrallet <sebastien.mestrallet@hey.com>
2024-01-26 12:07:42 +0100

    remove custom libHexEx

--- libHexEx/OpenVolumeMesh/src/Unittests/unittests.cc
+++
@@ -1,7 +0,0 @@
-#include "unittests_common.hh"
-
-int main(int _argc, char** _argv) {
-
-    testing::InitGoogleTest(&_argc, _argv);
-    return RUN_ALL_TESTS();
-}
--- libHexEx/cmake-library/CI/tests/Gurobi/test_cxx.cc
+++
@@ -1,8 +0,0 @@
-#include <gurobi_c++.h>
-
-int main(int argc, char *argv[])
-{
-    GRBEnv env;
-    GRBModel model {env};
-    return 0;
-}
